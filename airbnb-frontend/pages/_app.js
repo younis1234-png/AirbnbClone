f1b196bc31c1b9ba9b80b8557e4567b7ad94b247
Younis <younis@Youniss-MacBook-Pro.local>
2021-04-29 20:52:29 -0400

    first commit

---
+++ airbnb-frontend/pages/_app.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/Navbar";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
